519e1570ae5c8aab456bdfd822c555de4476312f
wdelcol12 <wdelcol@gmail.com>
2022-11-30 10:39:59 -0500

    final push

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
-//const CONNECTION_STRING = 'mongodb+srv://root:1122@cluster0.7nmmmgw.mongodb.net/?retryWrites=true&w=majority'
+
 
 
 mongoose.connect(CONNECTION_STRING);
